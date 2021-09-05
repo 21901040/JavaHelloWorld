5803d0dfd359f53c81de3d7a1e56b6966c9b9aac
21901040 <83258672+21901040@users.noreply.github.com>
2021-09-05 22:06:18 +0900

    age add

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,16 @@
-module Test {
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Main stepA = new Main();
+    stepA.a01();
+  }
+  void a01(){
+  	Scanner scan = new Scanner(System.in); 
+  	System.out.print("Enter a birth year > ");
+  	int birthyear = scan.nextInt();
+  	int age = 2021 - birthyear + 1;
+
+  	System.out.println("You are "+age+" years old.");
+  }
 }
